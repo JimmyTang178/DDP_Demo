909c46948d20ef650263b62f1d50680423703ea8
Jimmy tang <40620948+JimmyTang178@users.noreply.github.com>
2021-11-06 21:29:51 +0800

    initial

---
+++ utils.py
@@ -0,0 +1,25 @@
+import sys
+import torch
+import torchvision
+from tqdm import tqdm
+
+def evaluate_accuracy(data_iter, net, device=None):
+    print("evaluating...")
+    if device is None and isinstance(net, torch.nn.Module):
+        # 如果没指定device就使用net的device
+        device = list(net.parameters())[0].device
+    acc_sum, n = 0.0, 0
+    with torch.no_grad():
+        for X, y in tqdm(data_iter):
+            if isinstance(net, torch.nn.Module):
+                net.eval()  # 评估模式, 这会关闭dropout
+                acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
+                net.train()  # 改回训练模式
+            else:  # 自定义的模型, 3.13节之后不会用到, 不考虑GPU
+                if ('is_training' in net.__code__.co_varnames):  # 如果有is_training这个参数
+                    # 将is_training设置成False
+                    acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item()
+                else:
+                    acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+            n += y.shape[0]
+    return acc_sum / n
